e2d35a3dc419e605968f2068264e2b244e1f4396
gordon <7534199@qq.com>
2020-02-27 18:19:25 +0800

    设置运行时间为每天执行

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from time import sleep
 import sys
 import os
 
-from setting import capture_time, create_pdf_time
+from setting import (
+    capture_time, 
+    create_pdf_time,
+    name
+)
 
 SHOTS_DIRCTORY_NAME = 'shots'
 PDF_DIRCTORY_NAME = 'pdfs'
@@ -17,7 +21,7 @@ def capture_screen():
     print(datetime.now())
 
 
-def create_pdf_from_pics(path):
+def create_pdf_from_pics(path, name):
     file_start = datetime.today().strftime('%y%m%d')
     pics =[p for p in os.listdir(path) if p.endswith('.png') and p.startswith(file_start)]
     pics.sort()
@@ -25,7 +29,7 @@ def create_pdf_from_pics(path):
     for pic in pics:
         images.append(Image.open(os.path.join(path,pic)).convert('RGB'))
 
-    pdf_file = os.path.join(PDF_DIRCTORY_NAME,file_start+'.pdf')
+    pdf_file = os.path.join(PDF_DIRCTORY_NAME,name + file_start+'.pdf')
     if len(images) > 1:
         images[0].save(pdf_file, save_all=True, append_images=images[1:])
     else:
@@ -36,20 +40,28 @@ def create_pdf_from_pics(path):
     
     print('pdf文件创建完成')
 
+
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
 
     # 生成每个时间点的截屏任务
     today = datetime.today()   
     for t in capture_time:
-        task_time = today.replace(hour=int(t.split(':')[0]), minute=int(t.split(':')[1]), second=0, microsecond=0)
-        scheduler.add_job(capture_screen, 'date', run_date=task_time)
+        scheduler.add_job(capture_screen, 'cron', 
+                            hour=int(t.split(':')[0]), 
+                            minute=int(t.split(':')[1]),
+                            second=int(t.split(':')[2])
+                        )
 
     # 生成创建pdf文件的任务
     picture_path = os.path.join(sys.path[0],SHOTS_DIRCTORY_NAME)
-    create_time = today.replace(hour=int(create_pdf_time.split(':')[0]), minute=int(create_pdf_time.split(':')[1]), second=0, microsecond=0)
-    scheduler.add_job(create_pdf_from_pics, 'date', run_date=create_time, args=(picture_path,))
-    
+    scheduler.add_job(create_pdf_from_pics, 'cron',
+                        hour=int(create_pdf_time.split(':')[0]), 
+                        minute=int(create_pdf_time.split(':')[1]), 
+                        second=int(create_pdf_time.split(':')[2]),
+                        args=(picture_path, name,)
+                    )
+                    
     # 启动所有任务
     scheduler.start()
 
